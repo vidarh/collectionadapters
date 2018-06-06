8f808826b04e9c71edbef75e26c5bffffc0efc37
Vidar Hokstad <vidar@hokstad.com>
2018-06-06 05:12:57 +0100

    Added hash_sequel adapter

--- spec/collectionadapters_spec.rb
+++ spec/collectionadapters_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Collectionadapters do
   it "has a version number" do
     expect(Collectionadapters::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
